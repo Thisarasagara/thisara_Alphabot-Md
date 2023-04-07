b3307dc92f32ee34e9309b464e7888196175a97a
Thisarasagara <129646464+Thisarasagara@users.noreply.github.com>
2023-04-07 15:21:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,16 +32,16 @@ global.language = english //change indonesia to english if you don't understand 
 // Other
 // Other
 global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
+global.ownername = "thisara" //nama kalian
 global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
+global.youtube = "" //bebas asal jan hapus
 global.myweb2 = "https://s.id/BOTGITHUB" //bebas
-global.email = "mdsandaru2@gmail.com" //bebas
+global.email = "sagarathisra8@gmail.com" //bebas
 global.region = "Buddhist" //bebas
 global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
+global.premium = ['94773530294'] //premium user
+global.owner = ["94782901170", "94785771203"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "773530294" // nomor wa kalian
 global.ownernomerr = "+94764497078" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
